7377c30292ef508f54250d9998aeec98b53d2352
javiagu13 <javiagu13@gmail.com>
2020-08-14 16:41:20 +0200

    acabado redireccionamiento a producto

--- js/productDetails.js
+++ js/productDetails.js
@@ -1,15 +1,22 @@
 //Search
+function llamada(){
+	var prodName=window.location.search.substr(1);
+	prodName = prodName.replace('=','');
+	console.log(prodName);
+	
+	ocultar(prodName);
+}
 function ocultar(prodName){
-	/*var arrayOfElements=document.getElementsByClassName("Productos");
+	var arrayOfElements=document.getElementsByClassName("Productos");
 	
 	for (var i=0; i<arrayOfElements.length;i++){
 		var className = arrayOfElements[i].getAttribute("name");
 		var str = String(className).toLowerCase();
 		if(str.includes(prodName.toLowerCase())){
-			arrayOfElements[i].style.display="none";
-		}
-		else{
 			arrayOfElements[i].style.display="block";
 		}
-	}*/
+		else{
+			arrayOfElements[i].style.display="none";
+		}
+	}
 }
